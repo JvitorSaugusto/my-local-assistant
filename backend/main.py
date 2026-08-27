2fd2332ba5112e677baa816299572005d5d3bdd0
Jvitor <jvitorsaugusto@outlook.com>
2026-08-26 23:51:14 -0300

    feat: implementando celery e postgres assincrono

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from backend.agent.graph import build_graph
 from backend.api.controllers.chat_controller import chat_router
 from backend.api.controllers.ai_controller import ai_router
+from psycopg_pool import AsyncConnectionPool
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("Variável DATABASE_URL não encontrada!")
+
+DB_URI_LANGGRAPH = DATABASE_URL.replace("+asyncpg", "")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with AsyncConnectionPool(DB_URI_LANGGRAPH, kwargs={"autocommit": True}) as pool:
+        checkpointer = AsyncPostgresSaver(pool) # type: ignore
+        await checkpointer.setup()
+        
+        app.state.langgraph_checkpointer = checkpointer
+        
+        app.state.compiled_graph = build_graph().compile(checkpointer=checkpointer)
+        
+        yield
+        
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(ai_router, prefix="/ai", tags=["InteligenciaArtificial"])
 app.include_router(chat_router, prefix="/chats", tags=["ChatInteligenciaArtificial"])
---
+++ tasks.py
@@ -0,0 +1,29 @@
+# arquivo: backend/tasks.py
+from typing import cast
+from backend.main import DB_URI_LANGGRAPH
+from celery import Celery
+from langgraph.checkpoint.postgres import PostgresSaver
+from langchain_core.messages import HumanMessage
+from psycopg import Connection
+from langchain_core.runnables.config import RunnableConfig
+from backend.agent.graph import build_graph, State
+
+
+celery_app = Celery("assistente_tasks", broker="redis://localhost:6379/0", backend="redis://localhost:6379/0")
+
+@celery_app.task(bind=True)
+def run_langgraph_task(self, thread_id: str, user_input: str):
+    with Connection.connect(DB_URI_LANGGRAPH) as conn:
+        
+        checkpointer = PostgresSaver(conn) # type: ignore
+        
+        builder = build_graph() 
+        app_graph = builder.compile(checkpointer=checkpointer)
+        
+        config: RunnableConfig = {"configurable": {"thread_id": thread_id}}
+        inputs = cast(State, {"messages": [HumanMessage(content=user_input)]})
+        
+        app_graph.invoke(inputs, config=config)
+        
+    
+    return {"status": "concluido", "thread_id": thread_id, "mensagem": "Salvo no DB!"}
